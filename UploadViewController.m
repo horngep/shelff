0b2e6b9a307bc1ccb0125e69ac6febc717d49d79
horngep <horng.ep@hotmail.com>
2014-09-10 16:39:46 -0500

    3.5 enabled

--- UploadViewController.m
+++ UploadViewController.m
@@ -171,7 +171,7 @@
 
 - (void) animateTextField: (UITextField *)textField up: (BOOL) up
 {
-    const int movementDistance = 200; // tweak as needed
+    const int movementDistance = 210; // tweak as needed
     const float movementDuration = 0.3f; // tweak as needed
 
     int movement = (up ? -movementDistance : movementDistance);
